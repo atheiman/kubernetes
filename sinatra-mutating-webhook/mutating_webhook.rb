47f79abca0036cfdd2c99a4603548786715ede5b
Austin Heiman <atheimanksu@gmail.com>
2019-03-17 01:54:30 -0500

    logger

--- sinatra-mutating-webhook/mutating_webhook.rb
+++ sinatra-mutating-webhook/mutating_webhook.rb
@@ -8,17 +8,20 @@ end
 
 post '/fun-label', provides: 'application/json' do
   admission_review = JSON.parse(request.body.read)
-
+  uid = admission_review['request']['uid']
+  logger.info { "Processing request uid '#{uid}'" }
   resp = {
     response: {
-      uid: admission_review['request']['uid'],
-      allowed: true
+      uid: uid,
+      allowed: true # Allow all requests, this could be extended to reject certain requests
     }
   }
 
   # This annotation excludes pods from getting the patch
+  exclude_annotation = 'mutating-webhook.example.com/exclude'
   annotations = admission_review['request']['object']['metadata']['annotations']
-  if annotations && annotations['mutating-webhook.example.com/exclude']
+  if annotations && annotations[exclude_annotation]
+    logger.info { "Excluding due to annotation '#{exclude_annotation}'" }
     return resp.to_json
   end
 
@@ -36,6 +39,8 @@ post '/fun-label', provides: 'application/json' do
     # Create the labels object with the new label
     patch = [{ op: 'add', path: '/metadata/labels', value: { fun: 'hello' } }]
   end
+
+  logger.info { "Patch: #{patch}" }
   resp[:response][:patch] = Base64.encode64(patch.to_json)
   resp[:response][:patchType] = 'JSONPatch'
 
